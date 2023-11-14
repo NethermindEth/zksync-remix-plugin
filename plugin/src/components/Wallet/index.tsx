b7aa404615cc4f60ba95776312336a0f77ce1ef1
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-11-14 10:57:10 -0500

    chore: remove rainbowkit references

--- plugin/src/components/Wallet/index.tsx
+++ plugin/src/components/Wallet/index.tsx
@@ -1,24 +1,18 @@
 /* eslint-disable multiline-ternary */
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useAccount, useNetwork } from 'wagmi'
 import React, { useContext, useEffect, useState } from 'react'
 
-import copy from 'copy-to-clipboard'
 import './wallet.css'
 import * as zksync from 'zksync-web3'
 import { ConnectionContext } from '../../contexts/ConnectionContext'
 import { useWalletClient } from 'wagmi'
-import { MdCopyAll } from 'react-icons/md'
-import {  trimStr } from '../../utils/utils'
 
 
 const Wallet = () => {
   const {isConnected} = useAccount()
   const { data: walletClient } = useWalletClient()
   const { setAccount, setProvider } = useContext(ConnectionContext)
-  const chainConnected = useNetwork()
-  const [showCopied, setCopied] = useState(false)
 
   useEffect(() =>  {
     if (walletClient ){
