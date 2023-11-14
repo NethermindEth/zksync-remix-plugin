0210387346faeb8edce8b9c2a0afd787ebf95bb0
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-11-14 10:40:45 -0500

    feat:walletconnect wallet sdk instead of rainbow

--- plugin/src/index.tsx
+++ plugin/src/index.tsx
@@ -3,39 +3,47 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider, darkTheme, Chain} from "@rainbow-me/rainbowkit";
-
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+
 import {zkSyncTestnet, zkSync} from 'viem/chains'
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect"
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { EIP6963Connector, walletConnectProvider } from "@web3modal/wagmi";
 
-const { chains, publicClient } = configureChains(
-  [zkSyncTestnet, zkSync],
-  [publicProvider()]
-);
+const projectId = '630093679339d9e6a59508feafbae4ce' // TODO who owns this? make sure nethermind owns this and move to a config file may be ?
 
+const { chains, publicClient } = configureChains([zkSyncTestnet, zkSync], [walletConnectProvider({ projectId }), publicProvider()]);
 
-const { connectors } = getDefaultWallets({
-  appName: 'Zk',
-  projectId: '630093679339d9e6a59508feafbae4ce', // only for walletconnect wallet, TODO is there a better place to store this id?
-  chains
-});
+const metadata = {
+  name: "zkSync remix plugin",
+  description: "zkSync remix plugin",
+  // url: "",
+  // icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
 
 const wagmiConfig = createConfig({
   autoConnect: false,
-  connectors,
+  connectors: [
+    new WalletConnectConnector({ chains, options: { projectId, showQrModal: false, metadata } }),
+    new EIP6963Connector({ chains }),
+    new InjectedConnector({ chains, options: { shimDisconnect: true } }),
+    new CoinbaseWalletConnector({ chains, options: { appName: metadata.name } }),
+  ],
   publicClient,
-})
+});
+
+createWeb3Modal({ wagmiConfig, projectId, chains })
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <WagmiConfig config={wagmiConfig}>
-    <RainbowKitProvider chains={chains} theme={darkTheme()}>
      <React.StrictMode>
       <App />
      </React.StrictMode> 
-     </RainbowKitProvider>
    </WagmiConfig>
 )
 
