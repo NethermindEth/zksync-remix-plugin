7f8e823ce929be5a01638fb3c29340b1a2ab81bd
Bohdan Ohorodnii <limposfeed@gmail.com>
2023-11-16 15:58:26 +0200

    feat: copy contract address button

--- plugin/src/components/DeployedContracts/index.tsx
+++ plugin/src/components/DeployedContracts/index.tsx
@@ -1,6 +1,6 @@
 // A component that reads the compiled contracts from the context and displays them in a select
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { getContractNameFromFullName, getShortenedHash } from '../../utils/utils'
 import FunctionalInput from '../FunctionalInput'
 import './deployedContracts.css'
@@ -8,6 +8,10 @@ import { useAtom, useAtomValue } from 'jotai'
 import { deployedContractsAtom, deployedSelectedContractAtom } from '../../atoms/deployedContracts'
 import * as D from '../../ui_components/Dropdown'
 import { BsChevronDown } from 'react-icons/bs'
+import copy from 'copy-to-clipboard'
+import { MdCopyAll } from 'react-icons/md'
+import { FaCheck } from 'react-icons/fa'
+import useRemixClient from '../../hooks/useRemixClient'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 
@@ -19,24 +23,52 @@ const DeployedContracts: React.FC = () => {
     setSelectedContract
   ] = useAtom(deployedSelectedContractAtom)
 
+  const { remixClient } = useRemixClient()
+
   const [dropdownControl, setDropdownControl] = React.useState(false)
+
+  const [copied, setCopied] = React.useState(false)
+
+  useEffect(() => {
+    if (copied) {
+      remixClient.call(
+        'notification' as any,
+        'toast',
+        '📋 Copied contract address to clipboard'
+      ).catch(console.error)
+    }
+  }, [copied])
 
   return (
     <>
       <D.Root open={dropdownControl} onOpenChange={(e) => { setDropdownControl(e) }}>
-        <D.Trigger>
-          <div className="flex flex-row justify-content-space-between align-items-center p-2 br-1 devnet-trigger-wrapper">
-            <label className='text-light text-sm m-0'>
-              {selectedContract != null
-                ? `${getContractNameFromFullName(selectedContract.contractName)}, ${getShortenedHash(selectedContract.address, 6, 4)}`
-                : 'No contract selected'}
-            </label>
-            <BsChevronDown style={{
-              transform: dropdownControl ? 'rotate(180deg)' : 'none',
-              transition: 'all 0.3s ease'
-            }} />
-          </div>
-        </D.Trigger>
+        <div className={"flex flex-row"}>
+          <D.Trigger>
+            <div className="w-100 flex flex-row justify-content-space-between align-items-center p-2 br-1 devnet-trigger-wrapper">
+              <label className='text-light text-sm m-0'>
+                {selectedContract != null
+                  ? `${getContractNameFromFullName(selectedContract.contractName)}, ${getShortenedHash(selectedContract.address, 6, 4)}`
+                  : 'No contract selected'}
+              </label>
+              <BsChevronDown style={{
+                transform: dropdownControl ? 'rotate(180deg)' : 'none',
+                transition: 'all 0.3s ease'
+              }} />
+            </div>
+          </D.Trigger>
+          <button
+            className="btn"
+            onClick={() => {
+              copy(selectedContract?.address ?? '')
+              setCopied(true)
+              setTimeout(() => {
+                setCopied(false)
+              }, 1000)
+            }}
+          >
+            {copied ? <FaCheck /> : <MdCopyAll />}
+          </button>
+        </div>
         <D.Portal>
           <D.Content>
             {contracts.map((contract, index) => {
