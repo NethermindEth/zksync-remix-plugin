408f1887f0e70e888aada79de97b3cc93fa55960
taco-paco <edwinswatpako@gmail.com>
2024-07-29 12:14:40 +0300

    fix: migrate old static config to flow config

--- api/docker_run.sh
+++ api/docker_run.sh
@@ -2,7 +2,6 @@
 
 # Note: This script needs to run from inside /api dir
 export PROMTAIL_BASE_DIR=$(pwd)
-
-grafana-agent-flow --config.expand-env=true --config.file ./configs/grafana-logs.config.yaml &
+grafana-agent-flow run ./configs/grafana-logs.config.river
 
 cargo run --release
