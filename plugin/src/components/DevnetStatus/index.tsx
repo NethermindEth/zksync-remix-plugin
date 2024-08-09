3a9c638e8684137a7c99831bd8330a268b5ae82f
Satyam Bansal <satyamsgsits1994@gmail.com>
2024-08-09 13:15:52 +0530

    Add Devnet status component

---
+++ plugin/src/components/DevnetStatus/index.tsx
@@ -0,0 +1,21 @@
+import { RxDotFilled } from 'react-icons/rx'
+import React from 'react'
+import { useAtomValue } from 'jotai'
+import { envAtom, isDevnetAliveAtom } from '@/atoms'
+
+export const DevnetStatus: React.FC = () => {
+  const env = useAtomValue(envAtom)
+  const isDevnetAlive = useAtomValue(isDevnetAliveAtom)
+
+  return (
+    <>
+      {env === 'wallet' ? (
+        <RxDotFilled size={'30px'} color="rebeccapurple" title="Wallet is active" />
+      ) : isDevnetAlive ? (
+        <RxDotFilled size={'30px'} color="lime" title="Devnet is live" />
+      ) : (
+        <RxDotFilled size={'30px'} color="red" title="Devnet server down" />
+      )}
+    </>
+  )
+}
