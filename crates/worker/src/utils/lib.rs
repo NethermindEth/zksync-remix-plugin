d4271d1fdb5381e03997b6826ccaffdd37d36f82
taco-paco <edwinswatpako@gmail.com>
2024-08-23 11:55:44 +0700

    feat: introduce worksapce and types crate

--- crates/worker/src/utils/lib.rs
+++ crates/worker/src/utils/lib.rs
@@ -1,6 +1,6 @@
 use std::path::{Path, PathBuf};
+use tracing::debug;
 use uuid::Uuid;
-use log::debug;
 
 pub const SOL_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/", "hardhat_env/workspaces/");
 pub const ZK_CACHE_ROOT: &str = concat!(
