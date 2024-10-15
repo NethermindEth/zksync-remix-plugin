c957cb5ccca5ede3f998f71b410876927982328c
Bohdan Ohorodnii <limposfeed@gmail.com>
2024-10-15 10:07:32 +0300

    feat: add new zksolc version

--- api/src/utils/lib.rs
+++ api/src/utils/lib.rs
@@ -23,11 +23,11 @@ pub const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
 
 pub const DURATION_TO_PURGE: u64 = 60 * 5; // 5 minutes
 
-pub const ZKSOLC_VERSIONS: [&str; 3] = ["1.5.3", "1.4.1", "1.4.0"];
+pub const ZKSOLC_VERSIONS: [&str; 3] = ["1.5.4", "1.4.1", "1.4.0"];
 
 pub const DEFAULT_SOLIDITY_VERSION: &str = "0.8.24";
 
-pub const DEFAULT_ZKSOLC_VERSION: &str = "1.4.1";
+pub const DEFAULT_ZKSOLC_VERSION: &str = "1.5.4";
 
 pub const ALLOWED_NETWORKS: [&str; 2] = ["sepolia", "mainnet"];
 
